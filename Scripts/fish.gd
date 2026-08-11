af49647e8de4d6e75c878d3e5cd2aa3b18a73145
noelle <finmaynard@proton.me>
2026-08-11 01:57:05 +0100

    lsdkfgjhdlsk

--- Scripts/fish.gd
+++ Scripts/fish.gd
@@ -11,18 +11,21 @@ func _ready() -> void:
 
 
 func on_caught() -> void:
-	# Stop all fish behaviour once it has been hooked.
 	set_process(false)
 	set_physics_process(false)
 
-	# Don't let it trigger any more collisions.
 	set_deferred("monitoring", false)
 	set_deferred("monitorable", false)
 
-func would_hit_obstacle(target_global_position: Vector2) -> bool:
-	var collision_shape := get_node_or_null(
-		"CollisionShape2D"
-	) as CollisionShape2D
+
+func would_hit_obstacle(
+	target_global_position: Vector2
+) -> bool:
+
+	var collision_shape := (
+		get_node_or_null("CollisionShape2D")
+		as CollisionShape2D
+	)
 
 	if collision_shape == null:
 		return false
@@ -34,24 +37,28 @@ func would_hit_obstacle(target_global_position: Vector2) -> bool:
 
 	query.shape = collision_shape.shape
 
-	# Start with the collision shape's current
-	# global transformation.
+	# Start with the fish collider where it currently is.
 	query.transform = collision_shape.global_transform
 
-	# Move that hypothetical shape to wherever
-	# the fish is trying to go.
-	var movement := target_global_position - global_position
+	# Move our hypothetical collider to where the fish
+	# is trying to swim.
+	var movement := (
+		target_global_position
+		- global_position
+	)
+
 	query.transform.origin += movement
 
-	# Layer 2 = terrain/rocks.
+	# Layer 2 = Terrain.
 	query.collision_mask = 2
 
 	query.collide_with_bodies = true
 	query.collide_with_areas = false
 
-	var hits := get_world_2d().direct_space_state.intersect_shape(
-		query,
-		1
+	var hits := (
+		get_world_2d()
+		.direct_space_state
+		.intersect_shape(query, 1)
 	)
 
 	return not hits.is_empty()
